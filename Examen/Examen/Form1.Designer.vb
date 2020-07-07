e3b3748a20e6d0b51a1ddb6d9f1fb034779b70b0
nahum lopez <nahumlop17@gmail.com>
2020-07-07 09:21:12 -0600

    Guardando

--- Examen/Examen/Form1.Designer.vb
+++ Examen/Examen/Form1.Designer.vb
@@ -22,29 +22,184 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.components = New System.ComponentModel.Container()
+        Me.BtnAgregar = New System.Windows.Forms.Button()
+        Me.btnmod = New System.Windows.Forms.Button()
+        Me.btnelim = New System.Windows.Forms.Button()
+        Me.btnBuscar = New System.Windows.Forms.Button()
+        Me.dtg = New System.Windows.Forms.DataGridView()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.ErrorProvid = New System.Windows.Forms.ErrorProvider(Me.components)
+        Me.mtxtID = New System.Windows.Forms.MaskedTextBox()
+        Me.txtPrecio = New System.Windows.Forms.TextBox()
+        Me.txtcantidad = New System.Windows.Forms.TextBox()
+        Me.cbCliente = New System.Windows.Forms.ComboBox()
+        Me.cbProducto = New System.Windows.Forms.ComboBox()
+        Me.mtxtFechaVenta = New System.Windows.Forms.MaskedTextBox()
+        CType(Me.dtg, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.ErrorProvid, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'Button1
+        'BtnAgregar
         '
-        Me.Button1.Location = New System.Drawing.Point(26, 93)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.BtnAgregar.Location = New System.Drawing.Point(696, 35)
+        Me.BtnAgregar.Name = "BtnAgregar"
+        Me.BtnAgregar.Size = New System.Drawing.Size(75, 23)
+        Me.BtnAgregar.TabIndex = 0
+        Me.BtnAgregar.Text = "Agregar"
+        Me.BtnAgregar.UseVisualStyleBackColor = True
+        '
+        'btnmod
+        '
+        Me.btnmod.Location = New System.Drawing.Point(696, 132)
+        Me.btnmod.Name = "btnmod"
+        Me.btnmod.Size = New System.Drawing.Size(75, 23)
+        Me.btnmod.TabIndex = 1
+        Me.btnmod.Text = "Modificar"
+        Me.btnmod.UseVisualStyleBackColor = True
+        '
+        'btnelim
+        '
+        Me.btnelim.Location = New System.Drawing.Point(696, 225)
+        Me.btnelim.Name = "btnelim"
+        Me.btnelim.Size = New System.Drawing.Size(75, 23)
+        Me.btnelim.TabIndex = 2
+        Me.btnelim.Text = "Eliminar"
+        Me.btnelim.UseVisualStyleBackColor = True
+        '
+        'btnBuscar
+        '
+        Me.btnBuscar.Location = New System.Drawing.Point(696, 314)
+        Me.btnBuscar.Name = "btnBuscar"
+        Me.btnBuscar.Size = New System.Drawing.Size(75, 23)
+        Me.btnBuscar.TabIndex = 3
+        Me.btnBuscar.Text = "Buscar"
+        Me.btnBuscar.UseVisualStyleBackColor = True
+        '
+        'dtg
+        '
+        Me.dtg.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dtg.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.dtg.Location = New System.Drawing.Point(3, 16)
+        Me.dtg.Name = "dtg"
+        Me.dtg.Size = New System.Drawing.Size(377, 329)
+        Me.dtg.TabIndex = 4
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.dtg)
+        Me.GroupBox1.Location = New System.Drawing.Point(301, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(383, 348)
+        Me.GroupBox1.TabIndex = 5
+        Me.GroupBox1.TabStop = False
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.mtxtFechaVenta)
+        Me.GroupBox2.Controls.Add(Me.cbProducto)
+        Me.GroupBox2.Controls.Add(Me.cbCliente)
+        Me.GroupBox2.Controls.Add(Me.txtcantidad)
+        Me.GroupBox2.Controls.Add(Me.txtPrecio)
+        Me.GroupBox2.Controls.Add(Me.mtxtID)
+        Me.GroupBox2.Location = New System.Drawing.Point(16, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(279, 345)
+        Me.GroupBox2.TabIndex = 6
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Información de Ventas"
+        '
+        'ErrorProvid
+        '
+        Me.ErrorProvid.ContainerControl = Me
+        '
+        'mtxtID
+        '
+        Me.mtxtID.Location = New System.Drawing.Point(7, 41)
+        Me.mtxtID.Mask = "00000"
+        Me.mtxtID.Name = "mtxtID"
+        Me.mtxtID.Size = New System.Drawing.Size(100, 20)
+        Me.mtxtID.TabIndex = 0
+        Me.mtxtID.ValidatingType = GetType(Integer)
+        '
+        'txtPrecio
+        '
+        Me.txtPrecio.Location = New System.Drawing.Point(7, 134)
+        Me.txtPrecio.Name = "txtPrecio"
+        Me.txtPrecio.Size = New System.Drawing.Size(100, 20)
+        Me.txtPrecio.TabIndex = 2
+        '
+        'txtcantidad
+        '
+        Me.txtcantidad.Location = New System.Drawing.Point(7, 186)
+        Me.txtcantidad.Name = "txtcantidad"
+        Me.txtcantidad.Size = New System.Drawing.Size(100, 20)
+        Me.txtcantidad.TabIndex = 3
+        '
+        'cbCliente
+        '
+        Me.cbCliente.FormattingEnabled = True
+        Me.cbCliente.Location = New System.Drawing.Point(6, 238)
+        Me.cbCliente.Name = "cbCliente"
+        Me.cbCliente.Size = New System.Drawing.Size(121, 21)
+        Me.cbCliente.TabIndex = 4
+        '
+        'cbProducto
+        '
+        Me.cbProducto.FormattingEnabled = True
+        Me.cbProducto.Location = New System.Drawing.Point(7, 290)
+        Me.cbProducto.Name = "cbProducto"
+        Me.cbProducto.Size = New System.Drawing.Size(121, 21)
+        Me.cbProducto.TabIndex = 5
+        '
+        'mtxtFechaVenta
+        '
+        Me.mtxtFechaVenta.Location = New System.Drawing.Point(7, 87)
+        Me.mtxtFechaVenta.Mask = "00/00/0000"
+        Me.mtxtFechaVenta.Name = "mtxtFechaVenta"
+        Me.mtxtFechaVenta.Size = New System.Drawing.Size(100, 20)
+        Me.mtxtFechaVenta.TabIndex = 6
+        Me.mtxtFechaVenta.ValidatingType = GetType(Date)
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Button1)
+        Me.ClientSize = New System.Drawing.Size(803, 372)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnBuscar)
+        Me.Controls.Add(Me.btnelim)
+        Me.Controls.Add(Me.btnmod)
+        Me.Controls.Add(Me.BtnAgregar)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.dtg, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.ErrorProvid, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents Button1 As Button
+    Friend WithEvents BtnAgregar As Button
+    Friend WithEvents btnmod As Button
+    Friend WithEvents btnelim As Button
+    Friend WithEvents btnBuscar As Button
+    Friend WithEvents dtg As DataGridView
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents cbProducto As ComboBox
+    Friend WithEvents cbCliente As ComboBox
+    Friend WithEvents txtcantidad As TextBox
+    Friend WithEvents txtPrecio As TextBox
+    Friend WithEvents mtxtID As MaskedTextBox
+    Friend WithEvents ToolTip1 As ToolTip
+    Friend WithEvents ErrorProvid As ErrorProvider
+    Friend WithEvents mtxtFechaVenta As MaskedTextBox
 End Class
--- Examen/Examen/Form1.vb
+++ Examen/Examen/Form1.vb
@@ -1,3 +1,70 @@
 ﻿Public Class Form1
+    Dim conexion As New conexion
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        conexion.conectar()
+        llenar()
+        dtg.DataSource = conexion.dv
+        llenarcb()
+    End Sub
 
+    Private Sub BtnAgregar_Click(sender As Object, e As EventArgs) Handles BtnAgregar.Click
+        Dim agregar As String = "insert into  values('" + mtxtID.Text + "','" + mtxtFechaVenta.Text + "','" + txtPrecio.Text + "','" + txtcantidad.Text + "','" + cbCliente.Text + "','" + cbProducto.Text + "')"
+        If (conexion.insert(agregar, mtxtID.Text)) Then
+            MessageBox.Show("Datos agregados exitosamente")
+            llenar()
+
+
+        Else
+            MessageBox.Show("Ya existe una venta con el numero   " + mtxtID.Text + "")
+        End If
+    End Sub
+
+    Private Sub btnmod_Click(sender As Object, e As EventArgs) Handles btnmod.Click
+        Dim update As String = "fechaVenta='" + mtxtFechaVenta.Text + "', precio='" + txtPrecio.Text + "', cantidad='" + txtcantidad.Text + "', cliente='" + +"', producto='" + +"'"
+        If (conexion.modificar("factura.Venta", update, "idVenta='" + mtxtID.Text + "'")) Then
+            MessageBox.Show("Se ha actualizado satisfactoreamente")
+            llenar()
+        Else
+            MessageBox.Show("Error al actualizar los datos")
+        End If
+    End Sub
+
+    Private Sub btnelim_Click(sender As Object, e As EventArgs) Handles btnelim.Click
+        If (conexion.eliminar("factura.Venta", " idVenta='" + mtxtID.Text + "'")) Then
+            MessageBox.Show("Dato eliminado satisfactoreamente")
+            llenar()
+
+        Else
+            MessageBox.Show("Error al eliminar datos")
+        End If
+    End Sub
+
+    Private Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+
+    End Sub
+
+    Private Sub dtg_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles dtg.CellContentClick
+        Dim dgv As DataGridViewRow = dtg.Rows(e.RowIndex)
+        mtxtID.Text = dgv.Cells(0).Value.ToString()
+        mtxtFechaVenta.Text = dgv.Cells(1).Value.ToString()
+        txtPrecio.Text = dgv.Cells(2).Value.ToString()
+        txtcantidad.Text = dgv.Cells(3).Value.ToString()
+        cbCliente.Text = dgv.Cells(4).Value.ToString()
+        cbProducto.Text = dgv.Cells(5).Value.ToString()
+
+    End Sub
+    Public Sub llenar()
+        conexion.Llenar("Select * from factura.Venta", "factura.Venta")
+        dtg.DataSource = conexion.ds.Tables("factura.Venta")
+    End Sub
+
+    Public Sub llenarcb()
+        conexion.llenarcbCliente()
+        cbCliente.DataSource = conexion.datos2.Tables("factura.cliente")
+        cbCliente.DisplayMember = "Nombre" + "Apellido"
+
+        conexion.llenarcb()
+        cbProducto.DataSource = conexion.datos2.Tables("factura.producto")
+        cbProducto.DisplayMember = "nombreProducto"
+    End Sub
 End Class
